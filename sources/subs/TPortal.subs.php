9324201d526e8aed34055a02822fa0b84c8f3109
tino <tinoest@gmail.com>
2021-01-08 15:09:57 +0000

    Moved css to TinyPortal Views

--- sources/subs/TPortal.subs.php
+++ sources/subs/TPortal.subs.php
@@ -110,21 +110,21 @@ function tpLoadCSS() {{{
 		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['theme_url'] . '/css/tp-style.css?'.TPVERSION.'" />';
     }
 	else {
-		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/tp-style.css?'.TPVERSION.'" />';
+		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="TinyPortal/Views/css/tp-style.css?'.TPVERSION.'" />';
     }
 
 	if(!empty($settings['default_theme_url']) && !empty($settings['theme_url']) && file_exists($settings['theme_dir'].'/css/tp-responsive.css')) {
 		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['theme_url'] . '/css/tp-responsive.css?'.TPVERSION.'" />';
     }
 	else {
-		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/tp-responsive.css?'.TPVERSION.'" />';
+		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="TinyPortal/Views/css/tp-responsive.css?'.TPVERSION.'" />';
     }
 
 	if(!empty($settings['default_theme_url']) && !empty($settings['theme_url']) && file_exists($settings['theme_dir'].'/css/tp-custom.css')) {
 		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['theme_url'] . '/css/tp-custom.css?'.TPVERSION.'" />';
     }
 	else {
-		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="' . $settings['default_theme_url'] . '/css/tp-custom.css?'.TPVERSION.'" />';
+		$context['html_headers'] .= '<link rel="stylesheet" type="text/css" href="TinyPortal/Views/css/tp-custom.css?'.TPVERSION.'" />';
     }
 	
 	if(!empty($context['TPortal']['padding'])) {
